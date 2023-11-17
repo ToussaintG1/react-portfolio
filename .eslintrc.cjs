fe64579a69c9fd314e9c03be9a50cb9fdaf84ed5
Toussaint Gilbert <toussaintdgilbert@gmail.com>
2023-11-16 21:16:16 -0600

    updated eslint, and remofed Webpack COnfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,59 +1,15 @@
 module.exports = {
-    parser: "@babel/eslint-parser",
-  "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended"
-  ],
-  "plugins": [ "react" ],
-  "env": {
-      "es6": true,
-      "node": true
-  },
- 
-  "parserOptions": {
-    requireConfigFile: false,
-    babelOptions: {
-      babelrc: false,
-      configFile: false,
-      // your babel options
-      presets: ["@babel/preset-env"],
-      "ecmaVersion": 2018,
-      "sourceType": "module",
-      "ecmaFeatures": {
-          "jsx": true
-      }
-  },
-  babelOptions: {
-    configFile: "path/to/config.js",
-  },
-  "settings": {
-      "react": {
-          "pragma": "React",
-          "version": "detect"
-      }
-  },
-  "rules": {
-      "max-len": [1,1200,2,{"ignoreComments": true}],
-      "no-console":[1],
-      "no-unused-vars":[1],
-      "react/jsx-uses-vars": [2],
-      "no-loop-func":[1],
-      "indent": [
-          "error",
-          "tab"
-      ],
-      "linebreak-style": [
-          "error",
-          "unix"
-      ],
-      "quotes": [
-          "error",
-          "single"
-      ],
-      "semi": [
-          "error",
-          "always"
-      ],
-  },
-  },
-}
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+    ],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh'],
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+    },
+  }
